66006bf6e3c203f45d98155e2f0dadc61f54ee57
jgondina <jo.gonzalez@ufl.edu>
2022-07-27 15:48:14 -0400

    --

--- dowscaleFromL0/regrid_GLBy.py
+++ dowscaleFromL0/regrid_GLBy.py
@@ -13,8 +13,8 @@ def regrid_GLBy(src_grd, dst_grd, var, method='nearest_s2d', fillValue = 1e31):
     # plt.show()
 
     print(src_grd.hgrid.mask_rho)
-    srcCoords = {'lat': src_grd.hgrid.lat_rho, 'lon': src_grd.hgrid.lon_rho, 'mask': np.int(src_grd.hgrid.mask_rho)}
-    dstCoords = {'lat': dst_grd.hgrid.lat_rho, 'lon': dst_grd.hgrid.lon_rho, 'mask': np.int( dst_grd.hgrid.mask_rho)}
+    srcCoords = {'lat': src_grd.hgrid.lat_rho, 'lon': src_grd.hgrid.lon_rho, 'mask': src_grd.hgrid.mask_rho.astype(np.int)}
+    dstCoords = {'lat': dst_grd.hgrid.lat_rho, 'lon': dst_grd.hgrid.lon_rho, 'mask': dst_grd.hgrid.mask_rho.astype(np.int)}
 
     regrid = xesmf.Regridder(
         srcCoords, dstCoords,
