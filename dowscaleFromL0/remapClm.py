de7bad6cc14ca15d51852df64b95c4dbd012337e
jgondina <jo.gonzalez@ufl.edu>
2022-07-27 10:56:27 -0400

    --

--- dowscaleFromL0/remapClm.py
+++ dowscaleFromL0/remapClm.py
@@ -112,7 +112,8 @@ def remap_clm(src_file, src_varname, src_grd, dst_grd, dxy=20, cdepth=0, kk=0, d
 #       flooded = xr.DataArray(src_varz)
 #       flooded.to_netcdf("flooded.nc")
     else:
-        src_varz = pyroms.remapping.flood2d(src_var,src_grd)
+        src_varz = pyroms_toolbox.Grid_HYCOM.flood_fast(src_var, src_grd, pos=pos, spval=spval, \
+                                dxy=dxy, cdepth=cdepth, kk=kk)
 
 
     # horizontal interpolation using xesmf
