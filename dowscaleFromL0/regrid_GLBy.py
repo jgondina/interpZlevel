3c7d25f7885fb81237fb45725e568c698640be8c
jgondina <jo.gonzalez@ufl.edu>
2022-07-27 15:14:06 -0400

    --

--- dowscaleFromL0/regrid_GLBy.py
+++ dowscaleFromL0/regrid_GLBy.py
@@ -1,10 +1,15 @@
 import xarray as xr
 import xesmf
 import numpy as np
+import matplotlib.pyplot as plt
 
 def regrid_GLBy(src_grd, dst_grd, var, method='nearest_s2d', fillValue = 1e31):
 
     srcMask = (np.abs(var - fillValue) > 1e-10) | np.isnan(var)
+
+    plt.imshow(src_grd.hgrid.mask_rho)
+    plt.figure()
+    plt.imshow(dst_grd.hgrid.mask_rho)
 
     srcCoords = {'lat': src_grd.hgrid.lat_rho, 'lon': src_grd.hgrid.lon_rho, 'mask': 1 - src_grd.hgrid.mask_rho}
     dstCoords = {'lat': dst_grd.hgrid.lat_rho, 'lon': dst_grd.hgrid.lon_rho, 'mask': 1 - dst_grd.hgrid.mask_rho}
@@ -12,6 +17,7 @@ def regrid_GLBy(src_grd, dst_grd, var, method='nearest_s2d', fillValue = 1e31):
     regrid = xesmf.Regridder(
         srcCoords, dstCoords,
         method=method,
+        extrap_method = 'nearest_s2d',
         periodic=False,
         filename='regrid_t.nc',
         reuse_weights=False
