a5528bb5281f25b976d715fed07e40dd911b69d2
jgondina <jo.gonzalez@ufl.edu>
2022-07-27 13:22:43 -0400

    --

--- dowscaleFromL0/regrid_GLBy.py
+++ dowscaleFromL0/regrid_GLBy.py
@@ -18,8 +18,10 @@ def regrid_GLBy(src_grd, dst_grd, fld, method='nearest_s2d'):
         reuse_weights=False
     )
     print(fld)
+    fff = np.zeros(fld.shape)
+
     fld[np.abs(fld) > 1e10] = 0.0
     fld[np.isnan(fld)] = 0.0
     print(fld)
-    tdest = regrid(fld)
+    tdest = regrid(fff)
     return tdest
