a52ab84dd4cd84c10ff437b36705fa177e91a724
jgondina <jo.gonzalez@ufl.edu>
2022-07-27 14:58:16 -0400

    --

--- dowscaleFromL0/regrid_GLBy.py
+++ dowscaleFromL0/regrid_GLBy.py
@@ -4,7 +4,9 @@ import numpy as np
 
 def regrid_GLBy(src_grd, dst_grd, var, method='nearest_s2d', fillValue = 1e31):
 
-    srcCoords = {'lat': src_grd.hgrid.lat_rho, 'lon': src_grd.hgrid.lon_rho}
+    srcMask = (np.abs(var - fillValue) > 1e-10) | np.isnan(var)
+
+    srcCoords = {'lat': src_grd.hgrid.lat_rho, 'lon': src_grd.hgrid.lon_rho, 'mask': srcMask}
     dstCoords = {'lat': dst_grd.hgrid.lat_rho, 'lon': dst_grd.hgrid.lon_rho}
 
     regrid = xesmf.Regridder(
@@ -15,10 +17,10 @@ def regrid_GLBy(src_grd, dst_grd, var, method='nearest_s2d', fillValue = 1e31):
         reuse_weights=False
     )
 
-    # Fills nans and other invalid values.
-    var2 = np.zeros(var.shape)
-    var2[~np.isnan(var)] = var[~np.isnan(var)]
-    var2[np.abs(var - fillValue) > 1e-10] = 0.0
+    # # Fills nans and other invalid values.
+    # var2 = np.zeros(var.shape)
+    # var2[~np.isnan(var)] = var[~np.isnan(var)]
+    # var2[np.abs(var - fillValue) > 1e-10] = 0.0
 
     print(var)
     tdest = regrid(var2)
