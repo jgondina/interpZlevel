0ce44859c2fa5cd3ddc2ba4ec6a5f0928bc23abe
jgondina <jo.gonzalez@ufl.edu>
2022-07-27 15:35:29 -0400

    --

--- dowscaleFromL0/regrid_GLBy.py
+++ dowscaleFromL0/regrid_GLBy.py
@@ -7,10 +7,10 @@ def regrid_GLBy(src_grd, dst_grd, var, method='nearest_s2d', fillValue = 1e31):
 
     srcMask = (np.abs(var - fillValue) > 1e-10) | np.isnan(var)
 
-    plt.imshow(src_grd.hgrid.mask_rho)
-    plt.figure()
-    plt.imshow(dst_grd.hgrid.mask_rho)
-    plt.show()
+    # plt.imshow(src_grd.hgrid.mask_rho)
+    # plt.figure()
+    # plt.imshow(dst_grd.hgrid.mask_rho)
+    # plt.show()
 
     srcCoords = {'lat': src_grd.hgrid.lat_rho, 'lon': src_grd.hgrid.lon_rho, 'mask': src_grd.hgrid.mask_rho}
     dstCoords = {'lat': dst_grd.hgrid.lat_rho, 'lon': dst_grd.hgrid.lon_rho, 'mask': dst_grd.hgrid.mask_rho}
