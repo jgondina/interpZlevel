36f2bfea6ed2961eba2aca865101c0800671001e
jgondina <jo.gonzalez@ufl.edu>
2022-07-27 18:25:17 -0400

    --

--- dowscaleFromL0/regrid_GLBy.py
+++ dowscaleFromL0/regrid_GLBy.py
@@ -39,7 +39,7 @@ def regrid_GLBy(src_grd, dst_grd, var, method='nearest_s2d', fillValue = 1e31):
     print(var2[:,:].shape)
     print(var.sum())
     print(var2.sum())
-    tdest = regrid(var2)
+    tdest = regrid(var)
     print(tdest)
 
     return tdest
