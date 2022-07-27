dd9c9d443534482db11b3cdd1aafe19fc62fbdd2
jgondina <jo.gonzalez@ufl.edu>
2022-07-27 10:30:36 -0400

    --

--- dowscaleFromL0/regrid_GLBy.py
+++ dowscaleFromL0/regrid_GLBy.py
@@ -2,9 +2,9 @@ import xarray as xr
 import xesmf
 
 def regrid_GLBy(src_grd, dst_grd, fld, method='nearest_s2d'):
-    coords = xr.open_dataset('/import/AKWATERS/kshedstrom/gridpak/Arctic2/grid_Arctic_4.nc')
-    coords = coords.rename({'lon_rho': 'lon', 'lat_rho': 'lat'})
-    gsource = xr.open_dataset('/import/AKWATERS/kshedstrom/HYCOM/Svalbard/data/HYCOM_GLBy0.08_2018_345.nc')
+    # coords = xr.open_dataset('/import/AKWATERS/kshedstrom/gridpak/Arctic2/grid_Arctic_4.nc')
+    # coords = coords.rename({'lon_rho': 'lon', 'lat_rho': 'lat'})
+    # gsource = xr.open_dataset('/import/AKWATERS/kshedstrom/HYCOM/Svalbard/data/HYCOM_GLBy0.08_2018_345.nc')
 
     srcCoords = {'lat': src_grd.hgrid.lat_rho, 'lon': src_grd.hgrid.lon_rho}
     dstCoords = {'lat': dst_grd.hgrid.lat_rho, 'lon': dst_grd.hgrid.lon_rho}
