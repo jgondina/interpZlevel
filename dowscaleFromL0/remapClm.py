bc7a8285974b99029a784bb202e17b372f110262
jgondina <jo.gonzalez@ufl.edu>
2022-07-27 11:18:25 -0400

    --

--- dowscaleFromL0/remapClm.py
+++ dowscaleFromL0/remapClm.py
@@ -30,6 +30,8 @@ def remap_clm(src_file, src_varname, src_grd, dst_grd, dxy=20, cdepth=0, kk=0, d
     src_var = cdf.variables[src_varname][0]
     time = cdf.variables['ocean_time'][0]
 
+    print(cdf.variables[src_varname])
+
 
     # create IC file
     dst_file = src_file.rsplit('/')[-1]
