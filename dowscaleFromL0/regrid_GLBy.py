fc441a077bd81c3192443e87cc2bd3fb09f5c305
jgondina <jo.gonzalez@ufl.edu>
2022-07-28 16:44:51 -0400

    --

--- dowscaleFromL0/regrid_GLBy.py
+++ dowscaleFromL0/regrid_GLBy.py
@@ -31,12 +31,12 @@ def regrid_GLBy(src_grd, dst_grd, var, method='nearest_s2d', fillValue = 1e31):
     # Fills nans and other invalid values.
 
     var = var[:]
-    var2 = 1000+np.zeros(var.shape)
+    var2 = 10000000+np.zeros(var.shape)
     var2[~np.isnan(var)] = var[~np.isnan(var)]
     # var2[np.abs(var) > 1e-4] = 0.0
     # var2[:,:]=1.0
 
-    plt.imshow(var)
+    plt.imshow(var2)
 
 
     print(var2[:,:].shape)
