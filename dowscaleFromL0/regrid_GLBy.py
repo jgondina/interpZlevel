a54caa83e57a07de5f3ef223d22ac25bf1279c36
jgondina <jo.gonzalez@ufl.edu>
2022-07-28 17:05:04 -0400

    --

--- dowscaleFromL0/regrid_GLBy.py
+++ dowscaleFromL0/regrid_GLBy.py
@@ -32,8 +32,9 @@ def regrid_GLBy(src_grd, dst_grd, var, method='nearest_s2d', fillValue = 1e31):
 
     var = var[:]
     print(var)
-    var2 = 10000000+np.zeros(var.shape)
-    var2[~np.isnan(var)] = var[~np.isnan(var)]
+    # var2 = 10000000+np.zeros(var.shape)
+    # var2[~np.isnan(var)] = var[~np.isnan(var)]
+    var2 = var;
     # var2[np.abs(var) > 1e-4] = 0.0
     # var2[:,:]=1.0
 
