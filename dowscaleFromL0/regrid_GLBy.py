4937b4ecb9e0959a2c4b0970601f89a8e54e980a
jgondina <jo.gonzalez@ufl.edu>
2022-07-29 12:31:30 -0400

    --

--- dowscaleFromL0/regrid_GLBy.py
+++ dowscaleFromL0/regrid_GLBy.py
@@ -6,27 +6,23 @@ import matplotlib.pyplot as plt
 
 def regrid_GLBy(src_grd, dst_grd, var, method='nearest_s2d', fillValue = 1e31):
 
-    # srcMask = (np.abs(var - fillValue) > 1e-10) | np.isnan(var)
-    #
-    # plt.imshow(src_grd.hgrid.mask_rho)
-    # plt.figure()
-    # plt.imshow(dst_grd.hgrid.mask_rho)
-    # plt.show()
-
-    # print(np.sum(var[0,:,:][src_grd.hgrid.mask_rho.shape]))
+    # Computes the masks
+    srcMask = (np.abs(var - fillValue) < 1e-10*(np.abs(fillValue))) | np.isnan(var) | (src_grd.hgrid.mask_rho == 1.0)
+    dstMask = (np.abs(var - fillValue) < 1e-10*(np.abs(fillValue))) | np.isnan(var) | (src_grd.hgrid.mask_rho == 1.0)
 
     print(src_grd.hgrid.lat_rho.shape, src_grd.hgrid.mask_rho.shape)
     print(dst_grd.hgrid.lat_rho.shape, dst_grd.hgrid.mask_rho.shape)
-    srcCoords = {'lat': src_grd.hgrid.lat_rho, 'lon': src_grd.hgrid.lon_rho, 'mask': src_grd.hgrid.mask_rho.astype(np.int)}
+    srcCoords = {'lat': src_grd.hgrid.lat_rho, 'lon': src_grd.hgrid.lon_rho, 'mask': .astype(np.int)}
     dstCoords = {'lat': dst_grd.hgrid.lat_rho, 'lon': dst_grd.hgrid.lon_rho, 'mask': dst_grd.hgrid.mask_rho.astype(np.int)}
 
+    # Computes the regridder.
     regrid = xesmf.Regridder(
         srcCoords, dstCoords,
         method=method,
         extrap_method = 'nearest_s2d',
         periodic=False,
-        filename='regrid_t.nc',
-        reuse_weights=False
+        filename='./regrid_t.nc',
+        reuse_weights=True
     )
 
     # Converts a possible masked array into a regular one.
@@ -38,14 +34,8 @@ def regrid_GLBy(src_grd, dst_grd, var, method='nearest_s2d', fillValue = 1e31):
     # plt.imshow(var)
 
     # Performs the actual regridding
-    print(datetime.datetime.now())
     tdest = regrid(var)
-    print(datetime.datetime.now())
-    tdest = regrid(var)
-    print(datetime.datetime.now())
-    tdest = regrid(var)
-    print(datetime.datetime.now())
-    tdest = regrid(var)
+
     # plt.figure()
     # plt.imshow(tdest)
     # plt.show()
