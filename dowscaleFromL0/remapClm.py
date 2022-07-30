c9a0bcced69ea943e1714d9ce624caca9360e815
jgondina <jo.gonzalez@ufl.edu>
2022-07-29 21:41:58 -0400

    --

--- dowscaleFromL0/remapClm.py
+++ dowscaleFromL0/remapClm.py
@@ -286,7 +286,7 @@ def remapClimateUV2D(src_file, src_grd, dst_grd, dxy=20, cdepth=0, kk=0, dst_dir
     zlevel = -src_grd.vgrid.z_r[::-1,0,0]
     nzlevel = len(zlevel)
     dst_zcoord = pyroms.vgrid.z_coordinate(dst_grd.vgrid.h, zlevel, nzlevel)
-    print('PPPPPPPPP', dst_zcoord)
+    print('PPPPPPPPP', dst_zcoord[:].shape)
     dst_grdz = pyroms.grid.ROMS_Grid(dst_grd.name+'_Z', dst_grd.hgrid, dst_zcoord)
 
     # create variables in destination file
