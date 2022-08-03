8bf07f3db3c9d6e677779dc3bd9e2f38f9a9b748
jgondina <jo.gonzalez@ufl.edu>
2022-08-03 11:11:06 -0400

    --

--- dowscaleFromL0/remapClm.py
+++ dowscaleFromL0/remapClm.py
@@ -245,10 +245,10 @@ def remapClimate3D(src_file, src_varname, src_grd, dst_grd, dst_dir='./', idxTim
 
     # vertical interpolation from standard z level to sigma
     print('vertical interpolation from standard z level to sigma')
-    print(dst_grd.vgrid.__dict__)
-    print('HHHHHH', dst_grdz.vgrid.z[:,200,200])
-    print('HHHHHH', dst_grdz.vgrid.z[:, 300, 300])
-    print('HHHHHH', dst_grd.vgrid.N, dst_grdz.vgrid.z[:, 500, 500])
+    # print(dst_grd.vgrid.__dict__)
+    print('HHHHHH', dst_grd.vgrid.z[:,200,200])
+    print('HHHHHH', dst_grd.vgrid.z[:, 300, 300])
+    print('HHHHHH', dst_grd.vgrid.N, dst_grd.vgrid.z[:, 500, 500])
     dst_var = pyroms.remapping.z2roms(dst_varz[::-1,:,:], dst_grdz,
                                       dst_grd, Cpos=Cpos, spval=spval, flood=False)
 
