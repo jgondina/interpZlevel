0f00fa5e932eb9478cf8aa5a362436c74a19e655
jgondina <jo.gonzalez@ufl.edu>
2022-08-08 12:03:14 -0400

    --

--- dowscaleFromL0/remapClm.py
+++ dowscaleFromL0/remapClm.py
@@ -169,8 +169,8 @@ def remapClimate3D(src_file, src_varname, src_grd, dst_grd, oceanTimes, dst_dir=
     src_var = cdf.variables[src_varname]
     spval = src_var._FillValue
 
-    print('XXXXXXXXXXXXXXXXXX',cdf.variables['ocean_time'])
-    print('XXXXXXXXXXXXXXXXXX', cdf.variables['ntime'])
+    print('XXXXXXXXXXXXXXXXXX',cdf.variables['ocean_time'][:])
+    print('XXXXXXXXXXXXXXXXXX', cdf.variables['ntimes'])
 
     if idxTime is None:
         idxTime = 0
