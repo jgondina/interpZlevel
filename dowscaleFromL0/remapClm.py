c2ed07566d102280e88a0d65e9f4e11d87c8ea7e
jgondina <jo.gonzalez@ufl.edu>
2022-08-09 14:12:25 -0400

    ...

--- dowscaleFromL0/remapClm.py
+++ dowscaleFromL0/remapClm.py
@@ -225,7 +225,7 @@ def remapClimate3D(src_file, src_varname, src_grd, dst_grd, oceanTimes, dst_dir=
 
     print('TTTTT', dst_grd.vgrid.__dict__)
     print('TTTTT', src_grd.vgrid.z_r[:].shape)
-    dst_zcoord = pyroms.vgrid.z_coordinate(dst_grd.vgrid.h, dst_grd.vgrid.z, dst_grd.vgrid.z.shape[0])
+    dst_zcoord = pyroms.vgrid.z_coordinate(dst_grd.vgrid.h, src_grd.vgrid.z, src_grd.vgrid.z_r.shape[0])
 
     # print('>>>>>>>', dst_zcoord.__dict__)
 
