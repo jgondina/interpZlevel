5bef09c6c00d55c2c0f2a2aec1ee4ab50a0d73ef
jgondina <jo.gonzalez@ufl.edu>
2022-08-09 15:35:42 -0400

    ...

--- dowscaleFromL0/remapClm.py
+++ dowscaleFromL0/remapClm.py
@@ -271,8 +271,8 @@ def remapClimate3D(src_file, src_varname, src_grd, dst_grd, oceanTimes, dst_dir=
 
     # land mask
     idx = np.where(dst_grd.hgrid.mask_rho == 0)
-    for n in range(dst_grd.vgrid.N):
-        dst_var[n, idx[0], idx[1]] = np.nan
+    # for n in range(dst_grd.vgrid.N):
+        # dst_var[n, idx[0], idx[1]] = np.nan
 
 
     # write data in destination file
