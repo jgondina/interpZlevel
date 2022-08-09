8f4156e0575888ad17e427b654986521e5d9618d
jgondina <jo.gonzalez@ufl.edu>
2022-08-09 09:00:36 -0400

    --

--- dowscaleFromL0/downscaleFromL0.py
+++ dowscaleFromL0/downscaleFromL0.py
@@ -205,7 +205,10 @@ for idxTime, time in enumerate(time):
 
     # print('Interpolating 2D + time variables')
     L0_zeta = remapClimate2D(L0_out, 'zeta', gridL0, gridL1, oceanTimes, dst_dir='./', idxTime = idxTime)
-    #
+
+    print(':::::', L0_zeta.shape)
+    print(':::::', gridL0.vgrid.h.shape)
+    print(':::::', gridL1.vgrid.h.shape)
 
     # L0_zr = setDepth(L0_Vtransform, L0_Vstretching, L0_theta_s, L0_theta_b, L0_hc, L0_N, 1, L0_h, zeta = L0_zeta)
     # print('ddddddddddd', L0_zr.shape)
