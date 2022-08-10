8b547da354bd3018527d52e1acc08b607648c6e0
jgondina <jo.gonzalez@ufl.edu>
2022-08-10 11:20:51 -0400

    ...

--- dowscaleFromL0/remapClm.py
+++ dowscaleFromL0/remapClm.py
@@ -248,8 +248,8 @@ def remapClimate3D(src_file, src_varname, src_grd, dst_grd, oceanTimes, dst_dir=
     print('vertical interpolation from standard z level to sigma')
 
 
-    # dst_var = z22roms(dst_varz[::-1, :, :], dst_grdz,
-    #                   dst_grd, Cpos=Cpos, spval=spval, flood=False)
+    dst_var = z22roms(dst_varz[::-1, :, :], dst_grdz,
+                      dst_grd, Cpos=Cpos, spval=spval, flood=False)
 
     dst_var = pyroms.remapping.z2roms(dst_varz[::-1, :, :], dst_grdz,
                           dst_grd, Cpos=Cpos, spval=spval, flood=False)
