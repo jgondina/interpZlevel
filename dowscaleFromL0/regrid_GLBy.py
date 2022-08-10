fd7f9ab9d874a5c14f1ad8cf0fa3278b3fdafb35
jgondina <jo.gonzalez@ufl.edu>
2022-08-10 10:13:22 -0400

    ...

--- dowscaleFromL0/regrid_GLBy.py
+++ dowscaleFromL0/regrid_GLBy.py
@@ -51,10 +51,11 @@ def regrid_GLBy(src_grd, dst_grd, var, method='nearest_s2d', fillValue = 1e31, v
 
     tdest = regrid(var)
 
-    print('KKKK', tdest.shape)
-    # print('KKKK', tdest[0, :, :].shape)
-    # plt.imshow(tdest[0,:,:])
-    # plt.show()
+    try:
+        plt.imshow(tdest[0,:,:])
+        plt.show()
+    except:
+        pass
 
     # try:
     #     plt.imshow(tdest[:, :])
